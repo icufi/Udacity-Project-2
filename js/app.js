fb18ad8909ae3e867776612de8733966ae9ce7b9
Max Girteit <icufishmg@gmail.com>
2021-05-11 11:18:30 -0700

    fixed indent for loop

--- js/app.js
+++ js/app.js
@@ -19,46 +19,48 @@ const navBar = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
 // create menu dynamically
-  let menu = () =>{
-    for (let i = 0; i < sections.length; i++) {
+let menu = () => {
+  for (let i = 0; i < sections.length; i++) {
     navBar.innerHTML += `<li class='menu__link'><a href='#${sections[i].id}'>${sections[i].dataset.nav}</a></li>`;
-    }
   }
+}
 
 // smooth scroll to sections from nav
-  let navScrollTo = () => {
-    for (let i = 0; i < navBar.childNodes.length; i++) {
+let navScrollTo = () => {
+  for (let i = 0; i < navBar.childNodes.length; i++) {
     navBar.childNodes[i].addEventListener('click', function (e) {
-        e.preventDefault();
-        const hash = e.target.innerHTML;
-        const scrollTarget = document.querySelector(`[data-nav="${hash}"]`);
-        scrollTarget.scrollIntoView({behavior: 'smooth', block: 'center'});
-    })};
-  }
+      e.preventDefault();
+      const hash = e.target.innerHTML;
+      const scrollTarget = document.querySelector(`[data-nav="${hash}"]`);
+      scrollTarget.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    })
+  };
+}
 
 // update nav elements and sections to active class dependent on their location in viewport
-  function elementActiveStates() {
-    const links = document.querySelectorAll('.menu__link');
-    let index = sections.length;
+function elementActiveStates() {
+  const links = document.querySelectorAll('.menu__link');
+  let index = sections.length;
 
-    while(--index && window.scrollY + 400 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 400 < sections[index].offsetTop) { }
 
-    links.forEach((link) => link.classList.remove('your-active-link'));
-    links[index].classList.add('your-active-link');
-    sections.forEach(element => {element.classList.remove('your-active-class');
+  links.forEach((link) => link.classList.remove('your-active-link'));
+  links[index].classList.add('your-active-link');
+  sections.forEach(element => {
+    element.classList.remove('your-active-class');
     sections[index].classList.add('your-active-class');
-    })
-  }
+  })
+}
 
 // load nav after page is loaded
-document.addEventListener('load', menu ());
+document.addEventListener('load', menu());
 
 
 // smooth scroll to sections
 navScrollTo();
 
 // change section and nav link to css active when in viewport
-setTimeout(() => window.addEventListener('scroll', function () {elementActiveStates();} ), 0);
+setTimeout(() => window.addEventListener('scroll', function () { elementActiveStates(); }), 0);
 
 //menu on mobile
 const navActive = () => {
